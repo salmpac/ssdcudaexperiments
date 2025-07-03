5e5700b7ae5728914b30dc953d9ccb716211ad8c
sergey dushenkov <ss.dushenkov@gmail.com>
2025-07-04 02:17:02 +0300

    added bilinear interpolation

--- cudaproj.h
+++ cudaproj.h
@@ -1,17 +1,27 @@
+#pragma once
+
 #include <gdal_priv.h>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <chrono>
+#include <cuda_runtime.h>
+#include <algorithm>
 
 using namespace std;
-using uchar = unsigned char;
+using utype = unsigned char;
+#define GDT_Type GDT_Byte
+
+const bool debug_info = false;
 
 struct image_info {
-    vector<uchar*> bands;
+    vector<utype*> bands;
     size_t width;
     size_t height;
 };
 
-
 void create_raster(const char* filename, image_info image);
 image_info get_raster(const char* filename);
 
-void resize_matrix(uchar* matrix, size_t height, size_t width, uchar* result_matrix, size_t desired_height, size_t desired_width);
+void resize_matrix_nearest(utype* matrix, size_t height, size_t width, utype* result_matrix, size_t desired_height, size_t desired_width);
+void resize_matrix_bilinear(utype* matrix, size_t height, size_t width, utype* result_matrix, size_t desired_height, size_t desired_width);
--- main.cu
+++ main.cu
@@ -1,23 +1,15 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
 #include "cudaproj.h"
 
-using namespace std;
-using uchar = unsigned char;
-
-int main() {
-    GDALAllRegister();
+void resize_some_images() {
 
     string source_filename = "image.tiff";
     string result_filename = "result.tiff";
     image_info img = get_raster(source_filename.c_str());
-    image_info img_new = {{}, 700, 400};
+    image_info img_new = {{}, 500, 500};
 
     for (size_t i = 0; i < img.bands.size(); ++i) {
-        img_new.bands.push_back(new uchar[img_new.width * img_new.height]);
-        resize_matrix(img.bands[i], img.height, img.width, img_new.bands[i], img_new.height, img_new.width);
+        img_new.bands.push_back(new utype[img_new.width * img_new.height]);
+        resize_matrix_bilinear(img.bands[i], img.height, img.width, img_new.bands[i], img_new.height, img_new.width);
     }
     create_raster(result_filename.c_str(), img_new);
 
@@ -25,4 +17,10 @@ int main() {
         delete[] img.bands[i];
         delete[] img_new.bands[i];
     }
+}
+
+int main() {
+    GDALAllRegister();
+    resize_some_images();
+    return 0;
 }
